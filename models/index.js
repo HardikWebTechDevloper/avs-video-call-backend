0acc09a7c1c76f72b9192adcd9ae026934c00bb7
Hardik Gadhiya <hardik.j@vibrant-info.com>
2023-05-22 12:11:56 +0530

    Added chat save and list APIs

--- models/index.js
+++ models/index.js
@@ -6,6 +6,8 @@ const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'test';
+console.log("env::::", env);
+
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 

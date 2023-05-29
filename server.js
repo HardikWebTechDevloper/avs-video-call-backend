31db587af303d617c6993e7f4a83f799dc90968a
Hardik Gadhiya <hardik.j@vibrant-info.com>
2023-05-29 17:05:27 +0530

    Added changes in chat functionality

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const loggedInUsers = [];
 // File Upload Using Socket
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/group-chat-attachments/');
+    cb(null, 'uploads/');
   },
   filename: function (req, file, cb) {
     cb(null, Date.now() + '-' + file.originalname);

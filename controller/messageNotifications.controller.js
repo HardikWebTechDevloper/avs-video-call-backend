fbf2fbb9e9b841a66d2feb7fda00fdf09e30f733
Hardik Gadhiya <hardik.j@vibrant-info.com>
2023-06-06 18:50:08 +0530

    Added user profile API changes

--- controller/messageNotifications.controller.js
+++ controller/messageNotifications.controller.js
@@ -3,3 +3,14 @@ const { apiResponse } = require('../helpers/apiResponse.helper');
 const HttpStatus = require('../config/httpStatus');
 const { Op, literal } = require("sequelize");
 
+module.exports.saveMessageNotification = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            (async () => {
+
+            })();
+        } catch (error) {
+
+        }
+    });
+}
